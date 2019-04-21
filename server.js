9dfe4919efce0543261cdb20a7b5b2c3a35187a5
Omnious <panjigemilang31298@gmail.com>
2019-04-21 10:00:54 +0700

    second commit try to deploy heroku web

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 8080
+
+app.use(express.static(__dirname));
+
+// routes
+app.get("/", (req, res) => {
+    res.render("index")
+});
+
+app.listen(port, () => {
+    console.log("running server ..")
+});
