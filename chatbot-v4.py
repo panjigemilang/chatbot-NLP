3fae6626a4bab17c4e4ae3975108e9413e23da56
Omnious <panjigemilang31298@gmail.com>
2019-05-03 17:22:46 +0700

    cleaning

--- chatbot-v4.py
+++ chatbot-v4.py
@@ -5,16 +5,16 @@ from bs4 import BeautifulSoup
 USER_AGENT = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
  
  
-def hasil(search_term, number_hasils, language_code):
-    assert isinstance(search_term, str), 'Search term must be a string'
-    assert isinstance(number_hasils, int), 'Number of hasils must be an integer'
-    escaped_search_term = search_term.replace(' ', '+')
+def hasil(kata_pencarian, jumlah_hasil, bahasa):
+    assert isinstance(kata_pencarian, str), 'Kata pencarian harus String.'
+    assert isinstance(jumlah_hasil, int), 'Jumlah hasil harus integer.'
+    escaped_kata_pencarian = kata_pencarian.replace(' ', '+')
  
-    google_url = 'https://www.google.com/search?q={}&num={}&hl={}'.format(escaped_search_term, number_hasils, language_code)
+    google_url = 'https://www.google.com/search?q={}&num={}&hl={}'.format(escaped_kata_pencarian, jumlah_hasil, bahasa)
     response = requests.get(google_url, headers = USER_AGENT)
     response.raise_for_status()
  
-    return search_term, response.text
+    return kata_pencarian, response.text
 
 def parse_html(keyword, html):
     soup = BeautifulSoup(html, 'html.parser')
@@ -25,15 +25,15 @@ def parse_html(keyword, html):
     for parser in hasil_block:
 
         link = parser.find('a', href=True)
-        title = parser.find('h3')
-        description = parser.find('span', attrs={'class': 'st'})
-        if link and title:
+        judul = parser.find('h3')
+        deskripsi = parser.find('span', attrs={'class': 'st'})
+        if link and judul:
             link = link['href']
-            title = title.get_text()
-            if description:
-                description = description.get_text()
+            judul = judul.get_text()
+            if deskripsi:
+                deskripsi = deskripsi.get_text()
             if link != '#':
-                doc.append({'keyword': keyword, 'rank': rank, 'title': title, 'description': description})
+                doc.append({'keyword': keyword, 'rank': rank, 'judul': judul, 'deskripsi': deskripsi})
                 rank += 1
         
     return doc
